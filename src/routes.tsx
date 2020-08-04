f719abcb8c8d9be9096f33bff34f583600e593ca
Kochiyama <marcelokochiyama2210@gmail.com>
2020-08-04 03:18:39 -0300

    Changed development environment, updating packages and solving toubles

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import Landing from './pages/Landing';
+import Landing from './pages/landing';
 import TeacherList from './pages/TeacherList';
 import TeacherForm from './pages/TeacherForm';
 
