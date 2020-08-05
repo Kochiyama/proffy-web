143c7e437dbb6f4910d9133da90f0993a1241bb2
Kochiyama <marcelokochiyama2210@gmail.com>
2020-08-05 10:51:18 -0300

    fix bug: selecting option value, in Select component

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -19,7 +19,7 @@ const Select: React.FC<SelectProps> = ({ label, name, options, ...rest }) => {
         <option value="" disabled hidden >Selecione uma opção</option>
 
         {options.map(option => {
-          return <option key={option.value} value="option.value">{option.label}</option>
+          return <option key={option.value} value={option.value}>{option.label}</option>
         })} 
       </select>
     </div>
