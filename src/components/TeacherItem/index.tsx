d5115e094ca89569b6804fe4166e6b0f1e6e01b8
Kochiyama <marcelokochiyama2210@gmail.com>
2020-08-05 19:14:35 -0300

    update teacher item to really show teacher info

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -2,33 +2,57 @@ import React from 'react';
 
 import whatssapIcon from '../../assets/images/icons/whatsapp.svg'
 import './styles.css'
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+  avatar: string;
+  bio: string;
+  cost: number;
+  id: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+
+  function createNewConnection() {
+    api.post("connections", {
+      user_id: teacher.id
+    })
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://avatars0.githubusercontent.com/u/26096036?s=460&u=646df6230922902d6e60ff5a4f0a6db98a63e7a0&v=4" alt="Marcelo Kochiyama"/>
+        <img src={teacher.avatar} alt="Marcelo Kochiyama"/>
         <div>
-          <strong>Marcelo Haruo Kochiyama</strong>
-          <span>Matemática</span>
+          <strong>{ teacher.name }</strong>
+          <span>{ teacher.subject }</span>
         </div>
       </header>
 
       <p>
-        Amo passar conhecimento das maneiras mais lógicas e compreensiveis possível.
-        <br /><br />
-        Mais de 300.000 alunos alavancados.
+        {teacher.bio}
       </p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 120,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a 
+          target="blank"
+          href={`https://wa.me/${teacher.whatsapp}`} 
+          onClick={createNewConnection}
+        >
           <img src={whatssapIcon} alt="Whatssap"/>
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
